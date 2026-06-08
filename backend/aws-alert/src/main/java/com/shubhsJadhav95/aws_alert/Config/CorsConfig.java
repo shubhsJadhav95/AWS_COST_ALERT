1c77d494888318df8ed691ae999700661d18c7b8
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-06-09 00:22:27 +0530

    docker compose added

--- backend/aws-alert/src/main/java/com/shubhsJadhav95/aws_alert/Config/CorsConfig.java
+++ backend/aws-alert/src/main/java/com/shubhsJadhav95/aws_alert/Config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React Vite
+                        .allowedOrigins("http://localhost:5173",
+                                "http://127.0.0.1:5173",
+                                "http://localhost:3000") // React Vite
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
