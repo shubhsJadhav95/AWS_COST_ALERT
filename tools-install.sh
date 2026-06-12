3f1a78b07178b8abceaff30a0a280d1ab6b24f94
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-06-12 15:01:45 +0530

    terraform initilized

--- tools-install.sh
+++ tools-install.sh
@@ -2,18 +2,19 @@
 # For Ubuntu 22.04
 # Intsalling Java
 sudo apt update -y
-sudo apt install openjdk-17-jre -y
-sudo apt install openjdk-17-jdk -y
-java --version
+
 
 # Installing Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian/jenkins.io-2026.key
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt-get update -y
-sudo apt-get install jenkins -y
+  sudo apt-get install fontconfig openjdk-21-jre -y
+  sudo apt-get install jenkins -y
 
 # Installing Docker 
 #!/bin/bash
